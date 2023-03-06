eaacfdafad9f2f46a371ba1a73d203f242e2ded2
Sajjad Ali <sajjad.ali@arpatech.com>
2023-03-06 15:04:50 +0500

    redirect url

--- js/script.js
+++ js/script.js
@@ -12,6 +12,6 @@ $(document).ready(function(){
   });
   $('form').submit(function(event) {
     event.preventDefault();
-    window.location = '/home.html'
+    window.location = '/todo-app/home.html'
   })
 });
