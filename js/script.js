5acdcc9d396d71a3f51a89f61c38fbc95c778aec
Sajjad Ali <sajjad.ali@arpatech.com>
2023-03-06 13:33:42 +0500

    Added sign in/up pages

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#introModal").modal('show')
+  $(".action-btn").click(function(){
+    const form = $(this).attr("data-form");
+    if (form === 'signUpForm') {
+      $(`#loginForm`).addClass('d-none')
+      $(`#signUpForm`).removeClass('d-none')
+    } else {
+      $(`#signUpForm`).addClass('d-none')
+      $(`#loginForm`).removeClass('d-none')
+    }
+  });
+  $('form').submit(function(event) {
+    event.preventDefault();
+    window.location = '/home.html'
+  })
+});
